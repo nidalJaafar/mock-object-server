efa8be335745031549a412c7a3bb066754081efe
Nidal Jaafar <nidaljaafar.nj@gmail.com>
2023-04-05 20:33:39 +0300

    first commit

---
+++ src/main/java/platform/media/mockobjectserver/MockObjectServerApplication.java
@@ -0,0 +1,13 @@
+package platform.media.mockobjectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockObjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockObjectServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/platform/media/mockobjectserver/MockObjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.media.mockobjectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockObjectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
