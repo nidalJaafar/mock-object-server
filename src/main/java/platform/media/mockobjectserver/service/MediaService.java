4ccfc344936c6a402afe8135d40a21d07f8a1fbf
Nidal Jaafar <nidaljaafar.nj@gmail.com>
2023-04-15 01:05:19 +0300

    added upload response and logging

--- src/main/java/platform/media/mockobjectserver/service/MediaService.java
+++ src/main/java/platform/media/mockobjectserver/service/MediaService.java
@@ -2,11 +2,12 @@ package platform.media.mockobjectserver.service;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.codec.multipart.FilePart;
+import platform.media.mockobjectserver.response.UploadResponse;
 import reactor.core.publisher.Mono;
 
 public interface MediaService {
 
-    Mono<String> upload(FilePart request);
+    Mono<UploadResponse> upload(FilePart request);
 
     Mono<FileSystemResource> download(String name);
 }
